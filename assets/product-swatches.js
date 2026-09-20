d3e3c229064820fde9a55873e42bf032fb7afdcf
Naqi-Haider <139011094+Naqi-Haider@users.noreply.github.com>
2026-09-21 01:06:12 +0500

    made the full product-swatch logic, rendered them, changing the swatches without refresh and popstate for reserving the loadProduct function

---
+++ assets/product-swatches.js
@@ -0,0 +1,94 @@
+class ProductSwatches {
+    constructor() {
+        this.init();
+        this.initHistory();
+    }
+
+    init() {
+        document.addEventListener('click', (event) => {
+            const swatch = event.target.closest('[data-product-url]');
+
+            if (!swatch) return;
+
+            event.preventDefault();
+
+            // this.loadProduct(swatch); is changed to url because of popstate
+            const url = swatch.dataset.productUrl;
+            this.loadProduct(url, true);
+        });
+    }
+
+    initHistory() {
+        window.addEventListener('popstate', () => {
+            const url = window.location.pathname;
+            this.loadProduct(url, false);
+        })
+    }
+
+
+    async loadProduct(url, updateHistory = false) {
+        // const url = swatch.dataset.productUrl;
+        console.log('Loading Url: ', url);
+
+        try {
+            // It returns null because MainProduct-main-product doesn't exist for the current product DOM
+            // So we use data-section attribute:
+            const currentProduct = document.querySelector('product-info[data-section]');
+
+            if (!currentProduct) {
+                throw new Error('Current product not found');
+            }
+
+            // Get the dynamic ID of the section:
+            const sectionId = currentProduct.dataset.section;
+            console.log('Section ID: ', sectionId);
+
+            // Request the same section for new product:
+            const response = await fetch(`${url}?sections=${sectionId}`);
+
+            if (!response.ok) {
+                throw new Error('Failed to load product');
+            }
+
+            const sections = await response.json();
+            console.log("Sections: ", sections);
+
+            const newProductHTML = sections[sectionId];
+
+            if (!newProductHTML) {
+                throw new Error(`Section "${sectionId}" was not returned`)
+            }
+
+            //Now we parse:
+            const parser = new DOMParser();
+            const documentHTML = parser.parseFromString(newProductHTML, 'text/html');
+            console.log(documentHTML);
+
+            //Shopify section response from response:
+            const newSection = documentHTML.querySelector(`#shopify-section-${sectionId}`);
+            const currentSection = document.querySelector(`#shopify-section-${sectionId}`);
+
+            console.log('New Section: ', newSection);
+            console.log('Current section: ', currentSection);
+
+            if (!currentSection || !newSection) {
+                throw new Error('Product section cant be found');
+            }
+
+            // Replace:
+            currentSection.replaceWith(newSection);
+
+            // Only create a history when the user clicks on a swatch:
+            if (updateHistory) {
+                // history.pushState changes the url of the browser without refresh
+                // to get the updated content we used popstate
+                history.pushState({}, '', url); 
+            }
+
+        } catch (error) {
+            console.error(error);
+        }
+    }
+}
+
+new ProductSwatches();
